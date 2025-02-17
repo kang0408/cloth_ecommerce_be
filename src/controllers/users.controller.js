ea26469f3846c80a7a249094b93f8d38275447a0
Kang0408 <trandanhkhang482004@gmail.com>
2025-02-17 16:19:25 +0700

    feat: implement CRUD users API - Create API to get all users - Create API to create a user - Create API to edit a user by id - Create API to delete a user by id

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,6 +1,41 @@
+const bcrypt = require("bcrypt");
+
 const User = require("../models/users.model");
 
+const paginationHandler = require("../helpers/pagination.helper");
+const sortHandler = require("../helpers/sort.helper");
 const { successResponse, errorResponse } = require("../helpers/response.helper");
+
+// [GET] api/v1/users
+module.exports.users = async (req, res) => {
+  try {
+    // Sort
+    const sort = sortHandler(req.query);
+
+    // Pagination
+    const paginationDefault = { currentPage: 1, limitPage: 5 };
+    const pageTotal = await User.countDocuments();
+    const paginationObject = paginationHandler(paginationDefault, pageTotal, req.query);
+
+    const users = await User.find()
+      .skip(paginationObject.offset)
+      .limit(paginationObject.limitPage)
+      .sort(sort)
+      .select("-__v -password");
+
+    return successResponse(
+      res,
+      {
+        users,
+        totalPages: paginationObject.totalPage,
+        currentPage: paginationObject.currentPage
+      },
+      "Get all users successfully"
+    );
+  } catch (error) {
+    return errorResponse(res, error);
+  }
+};
 
 // [GET] api/v1/users/profile
 module.exports.profile = async (req, res) => {
@@ -15,5 +50,60 @@ module.exports.profile = async (req, res) => {
     return successResponse(res, data, "Get profile successfully");
   } catch (error) {
     return errorResponse(res, error, 500, "Get profile failure");
+  }
+};
+
+// [POST] api/v1/users/create
+module.exports.create = async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    const user = await User.findOne({ email: email });
+    if (user) return errorResponse(res, error, 400, "Email is existed");
+
+    const defaultPassword = "Abc@12345";
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(defaultPassword, salt);
+
+    req.body.password = hashPassword;
+    const newUser = new User(req.body);
+    await newUser.save();
+
+    return successResponse(res, null, "Create user successfully");
+  } catch (error) {
+    return errorResponse(res, error);
+  }
+};
+
+// [POST] api/v1/users/edit/:id
+module.exports.edit = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const user = await User.findOne({ _id: id });
+    if (!user) return errorResponse(res, error, 400, "User does not exist");
+
+    await User.updateOne({ _id: id }, req.body);
+
+    return successResponse(res, null, "Edit user successfully");
+  } catch (error) {
+    return errorResponse(res, error);
+  }
+};
+
+// [DELETE] api/v1/users/delete/:id
+module.exports.delete = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const user = await User.findOne({ _id: id });
+    if (!user) return errorResponse(res, error, 400, "User does not exist");
+
+    await User.deleteOne({ _id: id });
+
+    return successResponse(res, null, "Delete user successfully");
+  } catch (error) {
+    return errorResponse(res, error);
   }
 };
--- src/routes/v1/users.routes.js
+++ src/routes/v1/users.routes.js
@@ -1,11 +1,20 @@
 const express = require("express");
 
 const controller = require("../../controllers/users.controller");
+const userValidate = require("../../validations/users.validation.js");
 
 const middleware = require("../../middlewares/auth.middleware.js");
 
 const router = express.Router();
 
+router.get("", controller.users);
+
 router.get("/profile", middleware.auth, controller.profile);
+
+router.post("/create", userValidate.create, controller.create);
+
+router.post("/edit/:id", userValidate.edit, controller.edit);
+
+router.delete("/delete/:id", controller.delete);
 
 module.exports = router;
